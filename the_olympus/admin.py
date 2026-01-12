0385e09bfe11a70f83388a35f53d136025524750
daxhoDev <developer.daxho@gmail.com>
2026-01-12 15:04:50 -0500

    feat: render plans on landing from model

--- the_olympus/admin.py
+++ the_olympus/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from the_olympus.models import Plan, Profile
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Plan)
--- the_olympus/models.py
+++ the_olympus/models.py
@@ -16,7 +16,8 @@ class Profile(AbstractUser):
 class Plan(models.Model):
     name = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    features = models.TextField()  # Comma-separated list of features
+    features = models.JSONField(default=list)
+    is_premium = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name
+        return f'{self.name} | ${self.price}'
