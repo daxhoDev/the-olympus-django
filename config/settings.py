34d5f8e26125833b59f57c587301e20687909523
daxhoDev <developer.daxho@gmail.com>
2026-01-12 14:12:37 -0500

    fix: AUTH_USER_MODEL and missing migrations

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-AUTH_USER_MODEL = 'the_olympus.User'
+AUTH_USER_MODEL = 'the_olympus.Profile'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
