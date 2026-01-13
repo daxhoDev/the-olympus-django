3cde3e3f91422d4809a33c2fe69ab9b3b816f457
daxhoDev <developer.daxho@gmail.com>
2026-01-12 21:09:18 -0500

    feat: login form

--- config/views.py
+++ config/views.py
@@ -1,32 +1,16 @@
-from the_olympus.models import Profile, Plan
+from config.forms import LoginForm, ProfileCreationForm
+from the_olympus.models import Profile
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
-
-class ProfileCreationForm(UserCreationForm):
-	email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'register-form__input'}))
-	plan = forms.ModelChoiceField(
-		queryset=Plan.objects.all(),
-		required=False,
-		empty_label="--Select a plan--",
-		widget=forms.Select(attrs={'class': 'register-form__input'})
-	)
-	
-	class Meta:
-		model = Profile
-		fields = ['username', 'email', 'plan', 'password1', 'password2']
-		widgets = {
-			'username': forms.TextInput(attrs={'class': 'register-form__input'}),
-		}
-	
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		self.fields['password1'].widget.attrs.update({'class': 'register-form__input'})
-		self.fields['password2'].widget.attrs.update({'class': 'register-form__input'})
+from django.contrib.auth.views import LoginView
 
 class SignupView(CreateView):
 	model = Profile
 	form_class = ProfileCreationForm
 	success_url = reverse_lazy('landing')
-	template_name = 'auth/signup.html'
+	template_name = 'registration/signup.html'
+
+class CustomLoginView(LoginView):
+	template_name = 'registration/login.html'
+	authentication_form = LoginForm
+	success_url = reverse_lazy('landing')
