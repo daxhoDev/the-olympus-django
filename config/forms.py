522c58ba580721ea9ff958ea5d8e17e39923d580
daxhoDev <developer.daxho@gmail.com>
2026-01-21 11:40:40 -0500

    fix: register profile as admin

--- config/forms.py
+++ config/forms.py
@@ -30,6 +30,7 @@ class ProfileCreationForm(UserCreationForm):
 		if self.token:
 			try:
 				invitation = Invitation.objects.get(token=self.token)
+				self.invitation_role = invitation.role
 				if invitation.role == 'admin':
 					# Para admin, ocultar bank_account y plan
 					del self.fields['bank_account']
@@ -39,13 +40,31 @@ class ProfileCreationForm(UserCreationForm):
 					self.fields['bank_account'].required = True
 					self.fields['plan'].required = True
 			except Invitation.DoesNotExist:
+				self.invitation_role = None
 				# Si no existe la invitación, mostrar los campos requeridos
 				self.fields['bank_account'].required = True
 				self.fields['plan'].required = True
 		else:
+			self.invitation_role = None
 			# Sin token, mostrar los campos requeridos
 			self.fields['bank_account'].required = True
 			self.fields['plan'].required = True
+
+	def save(self, commit=True):
+		user = super().save(commit=False)
+		user.email = self.cleaned_data['email']
+		
+		# Establecer is_superuser según el role de la invitación
+		if self.invitation_role == 'admin':
+			user.is_superuser = True
+			user.is_staff = True
+		else:
+			user.is_superuser = False
+			user.is_staff = False
+		
+		if commit:
+			user.save()
+		return user
 
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
