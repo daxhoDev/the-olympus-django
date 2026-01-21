720095a3fa5db119e63384bc866c2293fcc981af
daxhoDev <developer.daxho@gmail.com>
2026-01-21 10:56:43 -0500

    feat: Payment model

--- the_olympus/models.py
+++ the_olympus/models.py
@@ -23,3 +23,12 @@ class Plan(models.Model):
 
     def __str__(self):
         return f'{self.name} | ${self.price}'
+    
+class Payment(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Payment of ${self.amount} by {self.profile.username} on {self.payment_date}'
